3b77d5eed7132322f7bb4ab0ddc4d057df10dd9d
tigeraus <tigeraus@icloud.com>
2019-07-04 14:25:13 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pgs',
+    version='0.0.1.dev1',
+    description='Parallel grid search.',
+    url='https://github.com/Tigeraus/pgs',
+    author='Huzi Cheng',
+    author_email='hzcheng15@icloud.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='grid search optimization model selection',
+    packages=['pgs'],
+)
---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import sys 
+import numpy as np
+sys.path.append('..')
+from pgs import GridSearcher
+
+
+if __name__ == "__main__":
+    parameter_dict = {
+        'pa': [1, 3, 3, 1, 2, 3, 3, 1, 2, 3, 3, 1, 2, 3],
+        'pb': [9, 1, 1, 2, 3, 3, 1, 2, 3],
+        'pd': [9, 1, 1, 2, 3, 3, 1, 2, 3],
+        'pe':[np.array([1,2,3])]
+    }
+
+    class Model2:
+        def __init__(self, args):
+            self.args = args
+            pass
+
+        def run(self, x):
+            z = self.args['pa']+self.args['pb']+np.sum(self.args['pe'])
+            return x+z
+
+    gs = GridSearcher(Model2, parameter_dict, processes=13, verbose=True, interval=0.1)
+    res = gs.search(save=False)
+    print(res[0])
